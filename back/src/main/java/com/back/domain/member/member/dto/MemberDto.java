ae543f827fe1d7d0711193408f864fdc373eea29
leesm-225 <lshine12393@gmail.com>
2026-06-24 09:33:27 +0900

    14-1

--- back/src/main/java/com/back/domain/member/member/dto/MemberDto.java
+++ back/src/main/java/com/back/domain/member/member/dto/MemberDto.java
@@ -1,15 +1,23 @@
 package com.back.domain.member.member.dto;
 
 import com.back.domain.member.member.entity.Member;
+import org.springframework.lang.NonNull;
 
 import java.time.LocalDateTime;
 
 public record MemberDto(
-        int id,
-        LocalDateTime createDate,
-        LocalDateTime modifyDate,
-        String name
+        @NonNull int id,
+        @NonNull LocalDateTime createDate,
+        @NonNull LocalDateTime modifyDate,
+        @NonNull String name
 ) {
+    public MemberDto(int id, LocalDateTime createDate, LocalDateTime modifyDate, String name) {
+        this.id = id;
+        this.createDate = createDate;
+        this.modifyDate = modifyDate;
+        this.name = name;
+    }
+
     public MemberDto(Member member) {
         this(
                 member.getId(),
--- back/src/main/java/com/back/domain/member/member/dto/MemberWithUsernameDto.java
+++ back/src/main/java/com/back/domain/member/member/dto/MemberWithUsernameDto.java
@@ -1,23 +1,32 @@
 package com.back.domain.member.member.dto;
 
 import com.back.domain.member.member.entity.Member;
+import org.springframework.lang.NonNull;
 
 import java.time.LocalDateTime;
 
 public record MemberWithUsernameDto(
-        int id,
-        LocalDateTime createDate,
-        LocalDateTime modifyDate,
-        String username,
-        String name
+        @NonNull int id,
+        @NonNull LocalDateTime createDate,
+        @NonNull LocalDateTime modifyDate,
+        @NonNull String name,
+        @NonNull String username
 ) {
+    public MemberWithUsernameDto(int id, LocalDateTime createDate, LocalDateTime modifyDate, String name, String username) {
+        this.id = id;
+        this.createDate = createDate;
+        this.modifyDate = modifyDate;
+        this.name = name;
+        this.username = username;
+    }
+
     public MemberWithUsernameDto(Member member) {
         this(
                 member.getId(),
                 member.getCreateDate(),
                 member.getModifyDate(),
-                member.getUsername(),
-                member.getName()
+                member.getName(),
+                member.getUsername()
         );
     }
 }
